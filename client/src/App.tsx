3730f30e5b31c4f0361afc67c478efbf7570ea59
Nurbol Yerish <128018514+nurbolyerish@users.noreply.github.com>
2024-03-16 13:45:47 +0600

    test commit (#1)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn my name 
         </a>
       </header>
     </div>
